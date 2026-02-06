aeba128e2ff98680a0d23e54ddcdb779be39d089
advanetale <tururov@ya.ru>
2026-02-06 13:25:57 +0300

    Update sitemap.ts

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -6,7 +6,7 @@ export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
       url: "https://d-mine.net",
-      lastModified: now(),
+      lastModified: new Date("2026-02-06").toISOString(),
       changeFrequency: "always",
       priority: 1,
     },
