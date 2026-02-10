d9dc9d2edfbb11e693333a6f9a66be340f255b7c
advanetale <tururov@ya.ru>
2026-02-11 01:21:20 +0300

    Upd

--- src/components/ServerTimer/ServerTimer.tsx
+++ src/components/ServerTimer/ServerTimer.tsx
@@ -54,7 +54,7 @@ export function ServerTimer() {
   if (isExpired) {
     return (
       <div className={styles.timer}>
-        <div className={styles.title}>1 сезон завершен!</div>
+        <div className={styles.title}>Анонс 2 сезона уже скоро!</div>
       </div>
     );
   }
